b21089c6fb860654888235542c465e168069509e
IRVINPC\Irvin <marinc.irvin@gmail.com>
2021-07-10 07:29:11 -0500

    lista expediente + parte

--- src/java/pe/gob/pj/entities/Parte.java
+++ src/java/pe/gob/pj/entities/Parte.java
@@ -11,81 +11,94 @@ package pe.gob.pj.entities;
  */
 public class Parte {
 
-    int c_tipo_parte;                                //Tipo Parte
-    int c_tipo_persona;                                //Tipo Persona
-    int c_tipo_doc;                                //Tipo de Documento
-    int x_doc_id;                                //Documento de Identidad
-    int x_ape_paterno;                                //Apellido paterno
-    int x_ape_materno;                                //Apellido Materno
-    int x_nombres;                                //Nombres 
-    int x_razon_social;                                //Razon social
+    String c_tipo_parte;                                //Tipo Parte
+    String c_tipo_persona;                                //Tipo Persona
+    String c_tipo_doc;                                //Tipo de Documento
+    String x_doc_id;                                //Documento de Identidad
+    String x_ape_paterno;                                //Apellido paterno
+    String x_ape_materno;                                //Apellido Materno
+    String x_nombres;                                //Nombres 
+    String x_razon_social;                                //Razon social
     String f_nacimiento;                                //fecha de Nacimiento
-    int c_recurrente;                                //Parte si es Recurrente
+    String c_recurrente;                                //Parte si es Recurrente
 
     public Parte() {
     }
 
-    public int getC_tipo_parte() {
+    public Parte(String c_tipo_parte, String c_tipo_persona, String c_tipo_doc, String x_doc_id, String x_ape_paterno, String x_ape_materno, String x_nombres, String x_razon_social, String f_nacimiento, String c_recurrente) {
+        this.c_tipo_parte = c_tipo_parte;
+        this.c_tipo_persona = c_tipo_persona;
+        this.c_tipo_doc = c_tipo_doc;
+        this.x_doc_id = x_doc_id;
+        this.x_ape_paterno = x_ape_paterno;
+        this.x_ape_materno = x_ape_materno;
+        this.x_nombres = x_nombres;
+        this.x_razon_social = x_razon_social;
+        this.f_nacimiento = f_nacimiento;
+        this.c_recurrente = c_recurrente;
+    }
+
+    public String getC_tipo_parte() {
         return c_tipo_parte;
     }
 
-    public void setC_tipo_parte(int c_tipo_parte) {
+    public void setC_tipo_parte(String c_tipo_parte) {
         this.c_tipo_parte = c_tipo_parte;
     }
 
-    public int getC_tipo_persona() {
+    public String getC_tipo_persona() {
         return c_tipo_persona;
     }
 
-    public void setC_tipo_persona(int c_tipo_persona) {
+    public void setC_tipo_persona(String c_tipo_persona) {
         this.c_tipo_persona = c_tipo_persona;
     }
 
-    public int getC_tipo_doc() {
+    public String getC_tipo_doc() {
         return c_tipo_doc;
     }
 
-    public void setC_tipo_doc(int c_tipo_doc) {
+    public void setC_tipo_doc(String c_tipo_doc) {
         this.c_tipo_doc = c_tipo_doc;
     }
 
-    public int getX_doc_id() {
+    public String getX_doc_id() {
         return x_doc_id;
     }
 
-    public void setX_doc_id(int x_doc_id) {
+    public void setX_doc_id(String x_doc_id) {
         this.x_doc_id = x_doc_id;
     }
 
-    public int getX_ape_paterno() {
+    public String getX_ape_paterno() {
         return x_ape_paterno;
     }
 
-    public void setX_ape_paterno(int x_ape_paterno) {
+    public void setX_ape_paterno(String x_ape_paterno) {
         this.x_ape_paterno = x_ape_paterno;
     }
 
-    public int getX_ape_materno() {
+    public String getX_ape_materno() {
         return x_ape_materno;
     }
 
-    public void setX_ape_materno(int x_ape_materno) {
+    public void setX_ape_materno(String x_ape_materno) {
         this.x_ape_materno = x_ape_materno;
     }
 
-    public int getX_nombres() {
+    public String getX_nombres() {
         return x_nombres;
     }
 
-    public void setX_nombres(int x_nombres) {
+    public void setX_nombres(String x_nombres) {
         this.x_nombres = x_nombres;
     }
 
-    public int getX_razon_social() {
+    public String getX_razon_social() {
         return x_razon_social;
     }
 
-    public void setX_razon_social(int x_razon_social) {
+    public void setX_razon_social(String x_razon_social) {
         this.x_razon_social = x_razon_social;
     }
 
@@ -97,12 +110,16 @@ public class Parte {
         this.f_nacimiento = f_nacimiento;
     }
 
-    public int getC_recurrente() {
+    public String getC_recurrente() {
         return c_recurrente;
     }
 
-    public void setC_recurrente(int c_recurrente) {
+    public void setC_recurrente(String c_recurrente) {
         this.c_recurrente = c_recurrente;
     }
 
+   
+
+    
+    
 }
--- src/java/pe/gob/pj/entities/utils/UtilDB.java
+++ src/java/pe/gob/pj/entities/utils/UtilDB.java
@@ -8,6 +8,7 @@ package pe.gob.pj.entities.utils;
 import java.util.ArrayList;
 import java.util.List;
 import pe.gob.pj.entities.Expediente;
+import pe.gob.pj.entities.Parte;
 
 /**
  *
@@ -18,8 +19,30 @@ public class UtilDB {
     public static List<Expediente> getListExpediente() {
 
         List<Expediente> list = new ArrayList<>();
-        list.add(new Expediente("1002020012302221", "CASACION", "ORDINARIO LABORAL", "LABORAL", "CSJ CUSCO", "43809", "1289-2019", 53, "REMITASE A SALA", "PAGO DE REINTEGRO DE BONO JURISDICCIONAL", "12/11/2019", "12/11/2019", 1094, 2018, 0, "CUSCO", "CUSCO", "SALA SUPERIOR", "1 SALA LABORAL", "SENTENCIA DE VISTA", "CONFIRMARON SENTENCIA", "12/11/2019", 231, 1094, 2018, 0, "CUSCO", "CUSCO", "JUZGADO ESPECIALIZADO", "4 JUZGADO DE TRABAJO DEL CUSCO", "SENTENCIA", "FUNDADA EN PARTE LA DEMANDA", "12/11/2019", 129));
+
+        Expediente epx1 = new Expediente("1002020012302221", "CASACION", "ORDINARIO LABORAL", "LABORAL", "CSJ CUSCO", "43809", "1289-2019", 53, "REMITASE A SALA", "PAGO DE REINTEGRO DE BONO JURISDICCIONAL", "12/11/2019", "12/11/2019", 1094, 2018, 0, "CUSCO", "CUSCO", "SALA SUPERIOR", "1 SALA LABORAL", "SENTENCIA DE VISTA", "CONFIRMARON SENTENCIA", "12/11/2019", 231, 1094, 2018, 0, "CUSCO", "CUSCO", "JUZGADO ESPECIALIZADO", "4 JUZGADO DE TRABAJO DEL CUSCO", "SENTENCIA", "FUNDADA EN PARTE LA DEMANDA", "12/11/2019", 129);
+        epx1.setPartes(getListParte());
+        list.add(epx1);
         list.add(new Expediente("1002020012302222", "CASACION", "ORDINARIO LABORAL", "LABORAL", "CSJ CUSCO", "43809", "128987-2019", 53, "REMITASE A SALA", "PAGO DE REINTEGRO DE BONO JURISDICCIONAL", "12/11/2019", "12/11/2019", 1094, 2018, 0, "CUSCO", "CUSCO", "SALA SUPERIOR", "1 SALA LABORAL", "SENTENCIA DE VISTA", "CONFIRMARON SENTENCIA", "12/11/2019", 231, 1094, 2018, 0, "CUSCO", "CUSCO", "JUZGADO ESPECIALIZADO", "4 JUZGADO DE TRABAJO DEL CUSCO", "SENTENCIA", "FUNDADA EN PARTE LA DEMANDA", "12/11/2019", 129));
+
+        return list;
+    }
+
+    public static List<Parte> getListParte() {
+        //DEMANDANTE	DEMANDADO
+        //NATURAL	JURIDICA ESTATAL
+        //DNI	
+        //92828222	
+        //CHAVEZ	
+        //LUDEÑA	
+        //PEDRO ANTONIO	
+        //	EMPRESA PATROL
+        //24/9/1981	
+        //NO            SI
+
+        List<Parte> list = new ArrayList<>();
+        list.add(new Parte("DEMANDANTE", "NATURAL", "DNI", "92828222", "CHAVEZ", "LUDEÑA", "PEDRO ANTONIO", "", "24/9/1981", "NO"));
+        list.add(new Parte("DEMANDADO", "JURIDICA ESTATAL", "", "", "", "", " ", "EMPRESA PATROL", "24/9/1981", "SI"));
 
         return list;
     }
